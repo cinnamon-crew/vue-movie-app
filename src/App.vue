aec69e7327d0ff3a1e331c43e2f524ddad5641ce
cinnamon-crew <maestrocinna@gmail.com>
2022-10-02 23:53:01 +0900

    Create project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Header />
+  <RouterView />
+  <Footer />
+</template>
+
+<script>
+import Header from "~/components/Header.vue";
+import Footer from "~/components/Footer";
+
+export default {
+  components: { Header, Footer },
+};
+</script>
